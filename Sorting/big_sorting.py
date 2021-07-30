261a2bd88ec06d07168313723f93dee1580f3700
pankaj1707k <pankaj.1707k@gmail.com>
2021-07-30 19:46:06 +0530

    Sorting arrays with very large numbers

---
+++ Sorting/big_sorting.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+""" https://www.hackerrank.com/challenges/big-sorting/problem """
+
+def bigSorting(unsorted):
+    n = len(unsorted)
+    unsorted.sort(key = int)
+    return unsorted
+
+
+n = int(input().strip())
+
+unsorted = []
+
+for _ in range(n):
+    unsorted_item = input()
+    unsorted.append(unsorted_item)
+
+result = bigSorting(unsorted)
+
+for i in range(n):
+    print(unsorted[i])
