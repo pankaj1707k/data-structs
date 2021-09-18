c6cbb23f51f7bbf27e55feb3c535daca1cd469a2
pankaj1707k <pankaj.1707k@gmail.com>
2021-09-18 22:15:18 +0530

    Added comments and formatting

--- DynamicProgramming/the_coin_change_problem.py
+++ DynamicProgramming/the_coin_change_problem.py
@@ -2,24 +2,25 @@
 
 
 def getWays(n, c, m, memo={}):
-    if (n,m) in memo:
-        return memo[(n,m)]
-    
-    if n == 0:
-        return 1
-    if n < 0 or m <= 0:
+    if (n, m) in memo:
+        return memo[(n, m)]
+
+    if n == 0:   # sum is zero
+        return 1    # one way -> not to take any number
+    if n < 0 or m <= 0:    # sum is negative or number of coins to be used is at-most zero
         return 0
-    
-    memo[(n,m)] = getWays(n, c, m-1) + getWays(n-c[m-1], c, m)
-    return memo[(n,m)]
+
+    memo[(n, m)] = getWays(n, c, m - 1) + getWays(n - c[m - 1], c, m)
+    # (num of ways to get n with m nums) = (num of ways to get n excluding the m-th coin) + (num of ways to get n including the m-th coin at least once)
+    return memo[(n, m)]
 
 
 def main():
     n, m = list(map(int, input().rstrip().split()))
     c = list(map(int, input().rstrip().split()))
-    result = getWays(n,c,m)
+    result = getWays(n, c, m)
     print(result)
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     main()
