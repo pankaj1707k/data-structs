d737090fb99055496d389f924015ce651891b9aa
pankaj1707k <pankaj.1707k@gmail.com>
2021-12-07 12:10:26 +0530

    Cycle detection in undirected graph using DFS

---
+++ Graphs/cycle_detection_in_undirected_graph.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+from typing import *
+
+def dfs(vertex:int, parent:int)->bool:
+    visited[vertex] = True
+    cycle_exist = False
+    for child in graph[vertex]:
+        if visited[child]:
+            if child == parent:
+                continue
+            return True     # if child is visited and is not the parent of vertex => cycle found
+        cycle_exist = cycle_exist or dfs(child, vertex)
+    return cycle_exist
+
+# graph input
+n, e = map(int, input().split())
+graph = defaultdict(list)
+visited = defaultdict(bool)
+for i in range(e):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for v in range(1,n+1):
+    if visited[v]:
+        continue
+    if dfs(v,0):
+        print("TRUE")
+        break
+else:
+    print("FALSE")
