e06b6a25f26a96612532f7a935c2c73440d9baa2
pankaj1707k <pankaj.1707k@gmail.com>
2021-12-08 15:51:34 +0530

    Leetcode: 104

---
+++ Graphs/maximum_depth_of_binary_tree.py
@@ -0,0 +1,28 @@
+""" https://leetcode.com/problems/maximum-depth-of-binary-tree/ """
+
+from typing import *
+from collections import defaultdict
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+def maxDepth(root:Optional[TreeNode])->int:
+    depth = defaultdict(int)
+
+    def dfs(node:Optional[TreeNode]):
+        if node==None:  return
+        if node.left != None:
+            depth[node.left] = depth[node] + 1
+            dfs(node.left)
+        if node.right != None:
+            depth[node.right] = depth[node] + 1
+            dfs(node.right)
+    
+    depth[root] = 0
+    dfs(root)
+    if root == None:
+        return 0
+    return 1 + max(depth.values())
