3d4668073413dedbfd9e7eb30332f35e94d497e6
pankaj1707k <pankaj.1707k@gmail.com>
2021-12-14 13:37:35 +0530

    Leetcode: 938

---
+++ Graphs/range_sum_of_bst.py
@@ -0,0 +1,20 @@
+""" https://leetcode.com/problems/range-sum-of-bst/ """
+
+from typing import *
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+def rangeSumBST(root:Optional[TreeNode], low:int, high:int)->int:
+    def solve(node:Optional[TreeNode]):
+        if node==None: return 0
+        if node.val < low:
+            return solve(node.right)
+        if node.val > high:
+            return solve(node.left)
+        return node.val + solve(node.left) + solve(node.right)
+    
+    return solve(root)
