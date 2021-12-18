18c4540286a1e25bd53ea45dd52c9a217a3c1ffd
pankaj1707k <pankaj.1707k@gmail.com>
2021-12-18 13:52:05 +0530

    Code reformat

--- Graphs/path-etic_sums.py
+++ Graphs/path-etic_sums.py
@@ -7,23 +7,26 @@ sys.stdin = open("../../input.txt", "r")
 sys.stdout = open("../../output.txt", "w")
 input = sys.stdin.readline
 
+
 def dfs(vertex: int, parent: int):
-    if parent==0 or values[parent]==2:
+    if parent == 0 or values[parent] == 2:
         values[vertex] = 1
     else:
         values[vertex] = 2
     for child in tree[vertex]:
-        if child == parent: continue
+        if child == parent:
+            continue
         dfs(child, vertex)
+
 
 t = int(input())
 for _ in range(t):
     n = int(input())
     tree = defaultdict(list)
-    for __ in range(n-1):
+    for __ in range(n - 1):
         u, v = map(int, input().split())
         tree[u].append(v)
         tree[v].append(u)
-    values = [0] * (n+1)
-    dfs(1,0)
-    print(' '.join(map(str, values))[2:])
+    values = [0] * (n + 1)
+    dfs(1, 0)
+    print(" ".join(map(str, values))[2:])
