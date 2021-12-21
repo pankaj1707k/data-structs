d884cfb7c23f7afdfa3af1f52676272c18baad68
pankaj1707k <pankaj.1707k@gmail.com>
2021-12-21 16:17:02 +0530

    Zero path operations: Hackerearth

---
+++ Graphs/zero_path_operations.py
@@ -0,0 +1,23 @@
+""" https://www.hackerearth.com/practice/algorithms/graphs/breadth-first-search/practice-problems/algorithm/zero-path-a7d370fd/ """
+
+import sys
+
+sys.stdin = open("../../input.txt", "r")
+sys.stdout = open("../../output.txt", "w")
+input = sys.stdin.readline
+
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    degree = [0] * (n + 1)
+    for __ in range(n - 1):
+        u, v = map(int, input().split())
+        degree[u] += 1
+        degree[v] += 1
+    w = [0] + list(map(int, input().split()))
+    num_of_ops = 0
+    for v in range(1, n + 1):
+        if degree[v] > 1 and w[v] != 0:
+            num_of_ops += 1
+    print(num_of_ops)
