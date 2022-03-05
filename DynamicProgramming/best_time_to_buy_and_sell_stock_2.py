360d0976dee0d6124b041057c38fce024c2aa83b
pankaj1707k <pankaj.1707k@gmail.com>
2022-03-05 13:45:33 +0530

    Leetcode: 122 (Modified)

--- DynamicProgramming/best_time_to_buy_and_sell_stock_2.py
+++ DynamicProgramming/best_time_to_buy_and_sell_stock_2.py
@@ -2,54 +2,29 @@
 
 from typing import *
 
-def maxProfit(prices: List[int])->int:
-    # Method 1
+
+def maxProfit(prices: List[int]) -> int:
     n = len(prices)
-    if n==1:
-        return 0
-    buy, sell = 0,1
-    mp = 0
-    while buy<n-1 and sell<n:
-        if prices[sell] > prices[buy]:
-            if sell!=n-1 and prices[sell+1] < prices[sell]:
-                mp += prices[sell]-prices[buy]
-                buy = sell+1
-                sell += 2
-            elif sell == n-1:
-                mp += prices[sell]-prices[buy]
-                break
-            else:
-                sell += 1
+    memo = [[-1] * 2 for _ in range(n + 9)]
+
+    def solve(index: int, can_buy: int) -> int:
+        if index >= n:
+            return 0
+        if memo[index][can_buy] != -1:
+            return memo[index][can_buy]
+        case_1, case_2 = 0, 0
+        if can_buy:
+            case_1 = solve(index + 1, 0) - prices[index]
+            case_2 = solve(index + 1, 1)
         else:
-            buy = sell
-            sell += 1
-    
-    return mp
+            case_1 = solve(index + 1, 1) + prices[index]
+            case_2 = solve(index + 1, 0)
+        memo[index][can_buy] = max(case_1, case_2)
+        return memo[index][can_buy]
 
-    # Method 2
-    
-    # memo = {}   # key: (i,b), value: max_profit
-    # n = len(prices)
-    # def solve(i:int, b:bool)->int:
-    #     if i >= n:
-    #         return 0
-    #     if (i,b) in memo:
-    #         return memo[(i,b)]
-    #     if b:
-    #         branch_1 = solve(i+1, False) - prices[i]    # bought
-    #         branch_2 = solve(i+1, True)     # did not buy
-    #     else:
-    #         branch_1 = solve(i+1, True) + prices[i]   # sold
-    #         branch_2 = solve(i+1, False)    # did not sell
-    #     memo[(i,b)] = max(branch_1, branch_2)
-    #     return memo[(i,b)]
-    # return solve(0,True)
-        
+    return solve(0, 1)
 
-tests = [
-    [7,1,5,3,6,4],
-    [1,2,3,4,5],
-    [7,6,4,3,1]
-]
+
+tests = [[7, 1, 5, 3, 6, 4], [1, 2, 3, 4, 5], [7, 6, 4, 3, 1]]
 for t in tests:
     print(maxProfit(t))
