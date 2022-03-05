9a03633a4784491472905e8301f808574999b827
pankaj1707k <pankaj.1707k@gmail.com>
2022-03-05 14:02:34 +0530

    Leetcode: 122 (Alternative solutions added)

--- DynamicProgramming/best_time_to_buy_and_sell_stock_2.py
+++ DynamicProgramming/best_time_to_buy_and_sell_stock_2.py
@@ -2,7 +2,9 @@
 
 from typing import *
 
-
+# Recursive solution with memoization
+# Time complexity: O(n)
+# Space complexity: O(n)
 def maxProfit(prices: List[int]) -> int:
     n = len(prices)
     memo = [[-1] * 2 for _ in range(n + 9)]
@@ -23,6 +25,44 @@ def maxProfit(prices: List[int]) -> int:
         return memo[index][can_buy]
 
     return solve(0, 1)
+
+
+# Iterative solution using valley-peak approach
+# Time complexity: O(n)
+# Space complexity: O(1)
+def maxProfit2(prices: List[int]) -> int:
+    max_profit = 0
+    for i in range(1, len(prices)):
+        if prices[i] > prices[i - 1]:
+            max_profit += prices[i] - prices[i - 1]
+    return max_profit
+
+
+# Iterative solution 2
+# Time complexity: O(n)
+# Space complexity: O(1)
+def maxProfit3(prices: List[int]) -> int:
+    n = len(prices)
+    if n == 1:
+        return 0
+    buy, sell = 0, 1
+    mp = 0
+    while buy < n - 1 and sell < n:
+        if prices[sell] > prices[buy]:
+            if sell != n - 1 and prices[sell + 1] < prices[sell]:
+                mp += prices[sell] - prices[buy]
+                buy = sell + 1
+                sell += 2
+            elif sell == n - 1:
+                mp += prices[sell] - prices[buy]
+                break
+            else:
+                sell += 1
+        else:
+            buy = sell
+            sell += 1
+
+    return mp
 
 
 tests = [[7, 1, 5, 3, 6, 4], [1, 2, 3, 4, 5], [7, 6, 4, 3, 1]]
