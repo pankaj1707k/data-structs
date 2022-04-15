d28ef18974104b4f7d5cdf3cbbe3db50c6f18d97
pankaj1707k <pankaj.1707k@gmail.com>
2022-04-15 12:14:07 +0530

    Leetcode: 441 (added approach 3)

--- Miscellaneous/arranging_coins.py
+++ Miscellaneous/arranging_coins.py
@@ -7,19 +7,37 @@ def arrangeCoins(n: int) -> int:
     s = 0
     k = 0
     i = 1
-    while (s < n):
+    while s < n:
         s += i
         k += 1
         i += 1
     if s == n:
         return k
-    return k-1
+    return k - 1
+
 
 # Method 2: O(1)
 def arrange_coins(n: int) -> int:
-    d = 1 + 8*n
+    d = 1 + 8 * n
     k = (sqrt(d) - 1) // 2
     return int(k)
+
+
+# Method 3: O(log(n)) [Binary search]
+def arrange_coins_bs(n: int) -> int:
+    left = 0
+    right = n
+    while left <= right:
+        mid = (left + right) // 2
+        rows = mid * (mid + 1) // 2
+        if rows == n:
+            return mid
+        if rows > n:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return right
+
 
 t = int(input().strip())
 result = []
