8287aab38b13e3933ddef3206e583d1da2c91c0b
pankaj1707k <pankaj.1707k@gmail.com>
2022-06-14 17:42:37 +0530

    Leetcode: 102

---
+++ Graphs/binary_tree_level_order_traversal.py
@@ -0,0 +1,28 @@
+""" https://leetcode.com/problems/binary-tree-level-order-traversal/ """
+
+from typing import *
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        result = []
+        level = [root]
+        while level:
+            result.append([node.val for node in level])
+            next_level = []
+            for node in level:
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+            level = next_level
+        return result
