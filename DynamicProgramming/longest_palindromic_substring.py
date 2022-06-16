04073532c2d8bb105985483878528ef53a5ff664
pankaj1707k <pankaj.1707k@gmail.com>
2022-06-16 10:21:43 +0530

    Leetcode: 5 (alternate solution)

--- DynamicProgramming/longest_palindromic_substring.py
+++ DynamicProgramming/longest_palindromic_substring.py
@@ -48,3 +48,25 @@ def longest_palindrome_2(s: str) -> str:
                 lps = s[i : j + 1]
 
     return lps
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if not s:
+            return ""
+        start = end = 0
+        for i in range(len(s)):
+            len1 = self.expand(s, i, i)
+            len2 = self.expand(s, i, i + 1)
+            maxlen = max(len1, len2)
+            if maxlen > end - start:
+                start = i - (maxlen - 1) // 2
+                end = i + maxlen // 2
+
+        return s[start : end + 1]
+
+    def expand(self, s: str, left: int, right: int) -> int:
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return right - left - 1
